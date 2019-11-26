c23c4075a2b9d8f6f26c61ffebe2c4aacc1841c4
vxd7 <ykonpaku13@gmail.com>
2019-11-26 14:28:09 +0300

    Reworked tests for Question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  # Shoulda matchers
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  describe 'associations' do
+    it { should have_many :answers }
+  end
 
-  # it 'validates presence of title' do
-  #   question = Question.new(body: '123')
-  #   expect(question).to_not be_valid
-  # end
-
-  # it 'validates presence of body' do
-  #   question = Question.new(title: 'title')
-  #   expect(question).to_not be_valid
-  # end
+  describe 'attribute validations' do
+    context 'presence validations' do
+      it { should validate_presence_of :title }
+      it { should validate_presence_of :body }
+    end
+  end
 end
