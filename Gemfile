9688691fe8873dab1969cc640874b4600e94048f
vxd7 <ykonpaku13@gmail.com>
2019-11-30 03:14:39 +0300

    Added sign in feature test

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :test do
   gem 'webdrivers'
   gem 'shoulda-matchers'
   gem 'rails-controller-testing'
+  gem 'launchy'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to be able to sign in
+} do
+
+  # 'let' in unit tests
+  given(:user) { User.create!(email: 'user@test.com', password: '123456') }
+
+  # 'before' in unit tests
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong_email@test.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
