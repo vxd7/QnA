b4c91203d2f6a945bad373002c3ce9e7785da746
vxd7 <ykonpaku13@gmail.com>
2019-12-01 19:04:32 +0300

    Added associations to question answer and user models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  belongs_to :author, class_name: :User, foreign_key: :user_id
 
   validates :body, presence: true
+  validates :author, presence: true
 end
