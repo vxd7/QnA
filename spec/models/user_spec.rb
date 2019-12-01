70274d029db066311bfd255d27d2d0662f462392
vxd7 <ykonpaku13@gmail.com>
2019-12-01 18:28:53 +0300

    Added more assosiations to question and user unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:questions).dependent(:destroy) }
+    it { should have_many(:answers).dependent(:destroy) }
+  end
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 end
