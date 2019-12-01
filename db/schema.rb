2c99da100958a2f01f7984a56cbe1cde7b2bd726
vxd7 <ykonpaku13@gmail.com>
2019-12-01 19:04:14 +0300

    Generated migrations for question and answer authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_180453) do
+ActiveRecord::Schema.define(version: 2019_12_01_144818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,9 @@ ActiveRecord::Schema.define(version: 2019_11_29_180453) do
   create_table "answers", force: :cascade do |t|
     t.string "body"
     t.bigint "question_id", null: false
+    t.bigint "user_id", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -26,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_11_29_180453) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +45,6 @@ ActiveRecord::Schema.define(version: 2019_11_29_180453) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
 end
