ff096d9053295e2060a2e7cebbcce1547c1a19b9
vxd7 <ykonpaku13@gmail.com>
2019-12-01 05:00:39 +0300

    Added question show feature spec

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'User can view selected question', %q{
+  In order to view the slected question and all answers to this question
+  As an authenticated or unauthenticated user
+  I'd like to be able to visit the question page and view question with answers
+} do
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  given(:answers) { create_list(:answer, 3, question: question) }
+
+  scenario 'Authenticated user visits question page and views question and answers' do
+    sign_in(user)
+    visit question_path question
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    answers.each { |ans| expect(page).to have_content ans.body }
+  end
+
+  scenario 'Unauthenticated user visits question page and views question and answers' do
+    visit question_path question
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    answers.each { |ans| expect(page).to have_content ans.body }
+  end
+end
