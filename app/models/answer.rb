c63c0c1889af4cc77e5ce5835417ebcf5eab7367
vxd7 <ykonpaku13@gmail.com>
2019-12-02 23:35:55 +0300

    Passing class_name in models as string

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  belongs_to :author, class_name: :User, foreign_key: :user_id
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
 
   validates :body, presence: true
   validates :author, presence: true
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  belongs_to :author, class_name: :User, foreign_key: :user_id
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
 
   validates :title, :body, presence: true
   validates :author, presence: true
