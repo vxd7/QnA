7b96b6e5b044bd849e329d7726191348d9908d4d
vxd7 <ykonpaku13@gmail.com>
2019-12-02 23:30:35 +0300

    Added author_of? instance method to User
    And also unit test for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+
+  def author_of?(resource)
+    resource.author.id == id
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'model methods' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question) }
+    let(:user_question) { create(:question, author: user) }
+
+    it 'should correctly determine the authorship of the given resource' do
+      expect(user.author_of?(user_question)).to be true
+      expect(user.author_of?(question)).to be false
+    end
+  end
 end
