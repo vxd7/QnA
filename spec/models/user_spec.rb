6ebc55191998ceab8567170568daed0dc8f9d53a
vxd7 <ykonpaku13@gmail.com>
2019-12-03 23:28:38 +0300

    Changed syntax in user unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe 'model methods' do
+  describe '#author_of?' do
     let(:user) { create(:user) }
     let(:question) { create(:question) }
     let(:user_question) { create(:question, author: user) }
 
     it 'should correctly determine the authorship of the given resource' do
-      expect(user.author_of?(user_question)).to be true
-      expect(user.author_of?(question)).to be false
+      expect(user).to be_author_of(user_question)
+      expect(user).to_not be_author_of(question)
     end
   end
 end
