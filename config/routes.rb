3dd244a128fe510b1f99ebfc84a1bd0111fab4a0
vxd7 <ykonpaku13@gmail.com>
2019-12-06 14:52:48 +0300

    Changed answer routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'questions#index'
 
   resources :questions do
-    resources :answers, shallow: true, only: %i[new create show destroy update]
+    resources :answers, shallow: true
   end
 end
