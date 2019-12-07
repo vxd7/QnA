25daf94674ec4e8e721cda19f5dcb0cb57137f17
vxd7 <ykonpaku13@gmail.com>
2019-12-07 18:31:26 +0300

    Answer best answer validation model test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe Answer, type: :model do
 
   describe 'attribute validations' do
     it { should validate_presence_of :body }
+
+    describe 'best answer validation' do
+      let!(:answer) { create(:answer) }
+      it { should validate_uniqueness_of(:best_answer).scoped_to(:question_id)}
+    end
   end
 end
