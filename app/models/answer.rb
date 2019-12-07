2bda4bde2a2d7ed33dd7fe335d23c0c908c0b824
vxd7 <ykonpaku13@gmail.com>
2019-12-07 18:31:00 +0300

    Answer model validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,5 @@ class Answer < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
   validates :body, presence: true
+  validates :best_answer, uniqueness: { scope: :question_id }
 end
