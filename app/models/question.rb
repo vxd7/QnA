e5f237d2bed1f90333ff344d9409d74b1b0b7f78
vxd7 <ykonpaku13@gmail.com>
2019-12-15 05:49:09 +0300

    Attach multiple files

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
-  has_one_attached :file
   belongs_to :author, class_name: 'User', foreign_key: :user_id
+
+  has_many_attached :files
 
   validates :title, :body, presence: true
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe Question, type: :model do
       it { should validate_presence_of :body }
     end
 
-    it 'has one attached file' do
-      expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One)
+    it 'have many attached files' do
+      expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
     end
   end
 end
