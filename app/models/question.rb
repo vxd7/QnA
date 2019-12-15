c087ed2850f7a3d5a52adc750f4601378e8960fc
vxd7 <ykonpaku13@gmail.com>
2019-12-15 04:23:23 +0300

    Finished attach one file

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  has_one_attached :file
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
   validates :title, :body, presence: true
