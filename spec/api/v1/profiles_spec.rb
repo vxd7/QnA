2850af283d715b82a4246d4c270dccbdca41c034
vxd7 <ykonpaku13@gmail.com>
2020-01-14 04:55:10 +0300

    Shared examples for API Authorizable

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -5,16 +5,9 @@ describe 'Profiles API', type: :request do
                     "ACCEPT" => 'application/json' } }
 
   describe 'GET /api/v1/profiles/me' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', headers: headers
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', params: { access_token: '1234' }, headers: headers
-        expect(response.status).to eq 401
-      end
+    it_behaves_like 'API Authorizable' do
+      let(:api_path) { '/api/v1/profiles/me' }
+      let(:method) { :get }
     end
 
     context 'authorized' do
