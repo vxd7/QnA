9084dc3673918efafc1bab888198ff0b4f019c8c
vxd7 <ykonpaku13@gmail.com>
2020-01-15 05:11:36 +0300

    API for creating, editing and deleting answers

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  before_action :find_question, only: :index
-  before_action :find_answer, only: :show
-  authorize_resource class: false
+  before_action :find_question, only: %i[index create]
+  before_action :find_answer, only: %i[show update destroy]
+  authorize_resource
 
   def index
     @answers = @question.answers
@@ -12,7 +12,35 @@ class Api::V1::AnswersController < Api::V1::BaseController
     render json: @answer
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+    @answer.author = current_user
+
+    if @answer.save
+      render json: @answer
+    else
+      render json: {'success': false }
+    end
+  end
+
+  def update
+    if current_user.author_of?(@answer)
+      @answer.update(answer_params.except(:files))
+    end
+  end
+
+  def destroy
+    # Check if user is authorized to delete the question
+    if current_user.author_of?(@answer)
+      @answer.destroy
+    end
+  end
+
   private
+
+  def answer_params
+    params.require(:answer).permit(:body, files: [], links_attributes: [:id, :name, :url, :_destroy])
+  end
 
   def find_question
     @question = Question.find(params[:question_id])
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -68,5 +68,100 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/questions/:question_id/answers' do
+    let(:headers) { { "ACCEPT" => 'application/json' } }
+    let!(:question) { create(:question) }
+    let(:api_path) { api_v1_question_answers_path(question) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :post }
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let(:test_answer_attribs) { { 'body' => 'New test answer body' } }
+      let(:test_invalid_answer_attribs) { { 'body' => '' } }
+
+      it 'creates new answer for the given question' do
+        expect do
+          post api_path, params: { access_token: access_token.token, answer: attributes_for(:answer) }, headers: headers
+        end.to change(question.answers, :count).by(1)
+      end
+
+      it 'returns newly created answer on success' do
+        post api_path, params: { access_token: access_token.token, answer: test_answer_attribs }, headers: headers
+
+        expect(json['answer']['body']).to eq test_answer_attribs['body'].as_json
+      end
+
+      it 'returns success: false on fail' do
+        post api_path, params: { access_token: access_token.token, answer: test_invalid_answer_attribs }, headers: headers
+        expect(json['success']).to be false
+      end
+
+      it 'does not change answers count on fail' do
+        expect do
+          post api_path, params: { access_token: access_token.token, answer: test_invalid_answer_attribs }, headers: headers
+        end.to_not change(question.answers, :count)
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/answer/:id' do
+    let(:headers) { { "ACCEPT" => 'application/json' } }
+
+    let(:user) { create(:user) }
+    let!(:answer) { create(:answer, author: user) }
+
+    let(:new_asnwer_params) { {'body' => 'New answer body' } }
+    let(:api_path) { api_v1_answer_path(answer) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :patch }
+    end
+
+    context 'authorized' do
+      let(:access_token_author) { create(:access_token, resource_owner_id: user.id) }
+      let(:access_token_not_author) { create(:access_token) }
+
+      it 'changes the answer if the author of the request is the author of the answer' do
+        expect do
+          patch api_path, params: { access_token: access_token_author.token, answer: new_asnwer_params}, headers: headers
+          answer.reload
+        end.to change(answer, :body).to(new_asnwer_params['body'])
+      end
+
+      it 'does not change the answer attributes if the request author is not the author of the answer' do
+        RSpec::Matchers.define_negated_matcher :not_change, :change
+
+        expect do
+          patch api_path, params: { access_token: access_token_not_author.token, question: new_asnwer_params }, headers: headers
+        end.to not_change(answer, :body)
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/answer/:id' do
+    let(:headers) { { "ACCEPT" => 'application/json' } }
+
+    let(:user) { create(:user) }
+    let!(:answer) { create(:answer, author: user) }
+    let(:api_path) { api_v1_answer_path(answer) }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :delete }
+    end
+
+    context 'authorized' do
+      let(:access_token_author) { create(:access_token, resource_owner_id: user.id) }
+
+      it 'deletes the answer' do
+        expect do
+          delete api_path, params: { access_token: access_token_author.token }, headers: headers
+        end.to change(Answer, :count).by(-1)
+      end
+    end
+  end
 end
 
